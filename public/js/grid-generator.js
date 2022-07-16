3bf3606494fd4b3e39a7103ed79530fb60080201
David Arques <david.darqsa@gmail.com>
2022-07-16 22:56:48 +0200

    Created random cell generator

--- public/js/grid-generator.js
+++ public/js/grid-generator.js
@@ -1,14 +1,7 @@
-const gridGenerator = (gridWidth) => {
-  const grid = [];
-
-  for (let i = 0; i < gridWidth; i += 1) {
-    grid[i] = [null];
-    for (let j = 0; j < gridWidth; j += 1) {
-      grid[i][j] = 0;
-    }
+const grid = [];
+for (let i = 0; i < gridLength; i += 1) {
+  grid[i] = [null];
+  for (let j = 0; j < gridLength; j += 1) {
+    grid[i][j] = 0;
   }
-  grid[2][2] = 1;
-  console.log(grid);
-};
-
-export default gridGenerator;
+}
