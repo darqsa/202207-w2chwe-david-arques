6653b27ee59976e82d8827842f4da6a8b4b88bb2
David Arques <david.darqsa@gmail.com>
2022-07-17 12:24:18 +0200

    Added random cell module + next turn module

--- public/js/next-turn.js
+++ public/js/next-turn.js
@@ -1,9 +1,11 @@
 import canvasGrid from "./grid.js";
+import info from "./info.js";
 import getRandomCells from "./get-random-cells.js";
+
 const nexTurn = grid.map((gridArray) => [...gridArray]);
-for (let columns = 0; columns < gridLength; columns += 1) {
-  for (let rows = 0; rows < gridLength; rows += 1) {
-    const cell = randomCells[columns][rows];
+for (let columns = 0; columns < info.canvasInfo.gridLength(); columns += 1) {
+  for (let rows = 0; rows < info.canvasInfo.gridLength(); rows += 1) {
+    const cell = getRandomCells[columns][rows];
     for (let i = -1; i < 1; i += 1) {
       for (let j = -1; j < 1; j += 1) {
         if (i === 0 && j === 0) {
