6cac52259d4e800eebc7d9141c081bc1732eab93
David Arques <david.darqsa@gmail.com>
2022-07-17 18:57:24 +0200

    Solved dead cell test bug with new conditions at dead-cell.js

--- public/js/dead-cell-condition.test.js
+++ public/js/dead-cell-condition.test.js
@@ -1,32 +1,35 @@
 import deadCell from "./dead-cell-condition";
 
 describe("Given the deadCell function", () => {
-  describe("When it recieves 2", () => {
+  describe("When it recieves 0 and 3", () => {
     test("Then it should return 1", () => {
-      const number1 = 2;
+      const number1 = 0;
+      const number2 = 3;
       const expectedResult = 1;
 
-      const result = deadCell(number1);
+      const result = deadCell(number1, number2);
 
       expect(result).toBe(expectedResult);
     });
   });
-  describe("When it recieves 0", () => {
-    test("Then it should return 0", () => {
-      const number1 = 0;
-      const expectedResult = 0;
+  describe("When it recieves 1 and 2", () => {
+    test("Then it should return 1", () => {
+      const number1 = 1;
+      const number2 = 2;
+      const expectedResult = 1;
 
-      const result = deadCell(number1);
+      const result = deadCell(number1, number2);
 
       expect(result).toBe(expectedResult);
     });
   });
-  describe("When it recieves 4", () => {
+  describe("When it recieves 1 and 4", () => {
     test("Then it should return 0", () => {
-      const number1 = 4;
+      const number1 = 1;
+      const number2 = 4;
       const expectedResult = 0;
 
-      const result = deadCell(number1);
+      const result = deadCell(number1, number2);
 
       expect(result).toBe(expectedResult);
     });
